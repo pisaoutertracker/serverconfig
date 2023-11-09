4037ccc49d5cf683d58f25e98b27c58ffd9e30d9
arizzi <Andrea.Rizzi@cern.ch>
2023-11-09 17:35:12 +0100

    filter json

--- ble-mqtt/mqtt-ble.py
+++ ble-mqtt/mqtt-ble.py
@@ -6,21 +6,21 @@ from switchbotmeter import DevScanner
 broker = "192.168.0.45"
 brokerport = 1883
 
-
+goodkeys=["temp","humidity"]
 def ble_status():
-    try:
+#    try:
         for current_devices in DevScanner():
             for device in current_devices:
                 if device.temp:
                     mqqttclient = paho.Client("BLE")
                     mqqttclient.connect(broker, brokerport)
                     ret = mqqttclient.publish(
-                        f"/ble/{device.device.addr}", json.dumps(device.data)
+                        f"/ble/{device.device.addr}", json.dumps(dict(filter(lambda x: x[0] in goodkeys, device.data.items())))
                     )
                     print(ret)
-    except:
-        print("Error in loop", flush=True)
-        time.sleep(60)
+#    except:
+#        print("Error in loop", flush=True)
+#        time.sleep(60)
 
 
 if __name__ == "__main__":
