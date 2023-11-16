9f1f81a2a1729f28ac25cd7434adfba047b0a2aa
Filippo Cattafesta <filippo.cattafesta99@gmail.com>
2023-11-16 14:50:47 +0100

    Try/Except removed

--- coldroom-mqtt/mqtt-coldroom.py
+++ coldroom-mqtt/mqtt-coldroom.py
@@ -27,81 +27,65 @@ class thermal_status:
     def login_session(self):
         print("Logging in...")
         login_url = f"{self.base_url}/httpd_auth.fcgi/accounts/login/"
-        try:
-            login_response = self.session.get(
-                login_url, headers=self.headers, verify=False
-            )
-            print("Login successful with status code", login_response.status_code)
-            return login_response
-        except:
-            print("Login failed")
+        login_response = self.session.get(login_url, headers=self.headers, verify=False)
+        print("Login successful with status code", login_response.status_code)
+        return login_response
 
     def authenticate(self):
         print("Authenticating...")
         auth_url = f"{self.base_url}/httpd_auth.fcgi/accounts/authenticate/"
-        try:
-            login_data = {
-                "username": os.environ.get("THERMAL_USERNAME"),
-                "password": os.environ.get("THERMAL_PASSWORD"),
-            }
-        except:
-            print("Credentials not found")
 
-        try:
-            auth_headers = {
-                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-                "X-CSRFToken": self.session.cookies["csrftoken"],
-                "Referer": f"{self.base_url}/main.html",
-                "Origin": self.base_url,
-            }
-            self.session.headers.update(auth_headers)
-            auth_response = self.session.post(
-                auth_url, json=login_data, verify=False, cookies=self.session.cookies
+        login_data = {
+            "username": os.environ.get("THERMAL_USERNAME"),
+            "password": os.environ.get("THERMAL_PASSWORD"),
+        }
+
+        auth_headers = {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "X-CSRFToken": self.session.cookies["csrftoken"],
+            "Referer": f"{self.base_url}/main.html",
+            "Origin": self.base_url,
+        }
+        self.session.headers.update(auth_headers)
+        auth_response = self.session.post(
+            auth_url, json=login_data, verify=False, cookies=self.session.cookies
+        )
+        if auth_response.status_code == 200:
+            self.is_active = True
+            print(
+                "Authentication successful with status code",
+                auth_response.status_code,
             )
-            if auth_response.status_code == 200:
-                self.is_active = True
-                print(
-                    "Authentication successful with status code",
-                    auth_response.status_code,
-                )
-            return auth_response
-        except:
-            print("Authentication failed")
+        return auth_response
 
     def get_status(self):
         print("Getting status...")
         status_response_list = []
-        try:
-            for json_name in self.json_interesting_dict.keys():
-                url = f"{self.base_url}/spes.fcgi/{json_name}"
-                status_response = self.session.get(
-                    url,
-                    headers=self.headers,
-                    verify=False,
-                    cookies=self.session.cookies,
-                )
-                # print(status_response.json())
-                time.sleep(1)
-                status_response_list.append(status_response.json())
-            print("Status retrieved")
-            return status_response_list
-        except:
-            print("Error in getting status")
+        for json_name in self.json_interesting_dict.keys():
+            url = f"{self.base_url}/spes.fcgi/{json_name}"
+            status_response = self.session.get(
+                url,
+                headers=self.headers,
+                verify=False,
+                cookies=self.session.cookies,
+            )
+            # print(status_response.json())
+            time.sleep(1)
+            status_response_list.append(status_response.json())
+        print("Status retrieved")
+        return status_response_list
 
     def check_status(self):
-        try:
-            for cookie in self.session.cookies:
-                if cookie.name == "sessionid":
-                    expiration_time = cookie.expires
-                    # expiration_time = time.time() - 5  # 5s test
-                    if time.time() > expiration_time:
-                        self.is_active = False
-                    else:
-                        self.is_active = True
-            return self.is_active
-        except:
-            print("Error in checking status")
-            exit(1)
+        for cookie in self.session.cookies:
+            if cookie.name == "sessionid":
+                expiration_time = cookie.expires
+                # expiration_time = time.time() - 5  # 5s test
+                if time.time() > expiration_time:
+                    self.is_active = False
+                else:
+                    self.is_active = True
+        return self.is_active
+
 
 import paho.mqtt.client as paho
 import json
@@ -116,26 +100,21 @@ def loop():
     therm = thermal_status()
     therm.login_session()
     while True:
-        try:
-            active = therm.check_status()
-            if active:
-                status_list = therm.get_status()
-                for status, publish_topic in zip(status_list, publish_topic_list):
-                    print(json.dumps(status, indent=4), flush=True)
-                    mqttclient = paho.Client("COLDROOM")
-                    mqttclient.connect(broker, brokerport)
-                    ret = mqttclient.publish(
-                        f"/coldroom/{publish_topic}", json.dumps(status)
-                    )  # signle json dump?
-                    print(ret)
-                time.sleep(10)
-            else:
-                therm.authenticate()
-                time.sleep(5)
-
-        except:
-            print("Error in loop", flush=True)
-            time.sleep(60)
+        active = therm.check_status()
+        if active:
+            status_list = therm.get_status()
+            for status, publish_topic in zip(status_list, publish_topic_list):
+                print(json.dumps(status, indent=4), flush=True)
+                mqttclient = paho.Client("COLDROOM")
+                mqttclient.connect(broker, brokerport)
+                ret = mqttclient.publish(
+                    f"/coldroom/{publish_topic}", json.dumps(status)
+                )  # signle json dump?
+                print(ret)
+            time.sleep(10)
+        else:
+            therm.authenticate()
+            time.sleep(5)
 
 
 if __name__ == "__main__":
