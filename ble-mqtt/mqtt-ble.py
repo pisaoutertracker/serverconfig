b2f543de2d5198b6d55bc504e98ad95c858129bc
arizzi <andrea.rizzi@cern.ch>
2024-07-10 21:43:58 +0200

    add Sensiron CO2 support

--- ble-mqtt/mqtt-ble.py
+++ ble-mqtt/mqtt-ble.py
@@ -30,6 +30,7 @@ def ble_status():
     for current_devices in DevScanner():
         for device in current_devices:
             # Check if the device is ours
+            msg=""
             if device.temp and device.model == "T" and device.mode == "00":
                 # Check if we know the device name
                 if device.device.addr in names:
@@ -39,12 +40,31 @@ def ble_status():
                     logging.error(f"{device.data}")
                     raise KeyError
                 # Publish the data to MQTT
-                mqqttclient = paho.Client("BLE")
-                mqqttclient.connect(broker, brokerport)
-                # Only publish the keys we want
                 msg = json.dumps(
                     dict(filter(lambda x: x[0] in goodkeys, device.data.items()))
                 )
+            else:
+              if device is not None:
+                  for _, key, value in device.device.getScanData():
+                      if key=="Manufacturer" :
+                          hexv = binascii.unhexlify(value)
+                          raw=bytearray(hexv)
+                          if value[:4]=="d506": #Sensiron CO2 sensor
+                                if device.device.addr in names:
+                                     device_name = names[device.device.addr]
+                                else :
+                                    device_name= "Sensiron CO2 ble:"+device.device.addr
+
+                                temp_ticks, humidity_ticks, co2 = struct.unpack("<HHH", raw[6:12])
+                                if  temp_ticks !=0 and co2!= 0:
+                                   temp = -45 + ((175.0 * temp_ticks) / (2**16 - 1))
+                                   humidity = (100.0 * humidity_ticks) / (2**16 - 1)
+                                msg=f'"CO2":{co2},"temp":{temp},"humidiy":{humidity}'
+
+            if msg != "":    
+                mqqttclient = paho.Client("BLE")
+                mqqttclient.connect(broker, brokerport)
+                # Only publish the keys we want
                 ret = mqqttclient.publish(f"/ble/{device_name}", msg,retain=True,  properties=publish_properties)
                 logging.info(f"{device_name} ({device.device.addr}) {msg}")
 
